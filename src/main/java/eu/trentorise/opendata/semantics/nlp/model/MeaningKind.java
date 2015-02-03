0d412cd34cdd7b724092d96efa2e9da4255188c8
David Leoni <davidleonigit-github@yahoo.com>
2015-02-03 16:46:46 +0100

    better documented MeaningKind

--- src/main/java/eu/trentorise/opendata/semantics/nlp/model/MeaningKind.java
+++ src/main/java/eu/trentorise/opendata/semantics/nlp/model/MeaningKind.java
@@ -1,10 +1,22 @@
 package eu.trentorise.opendata.semantics.nlp.model;
 
 /**
+ * The kind of a meaning. 
+ * 
  * @author David Leoni <david.leoni@unitn.it>
  */
-public enum MeaningKind {    
+public enum MeaningKind {
+
+    /**
+     * An entity is something so important to us to be referred to it with a
+     * name, for example 'New York City', 'Albert Einstein', 'W3C'.
+     */
     ENTITY,
-    CONCEPT, 
-    UNKNOWN;       
+    /**
+     * A concept is a group of objects referred with a common name, for example
+     * 'city', 'person', 'organization'.
+     */
+    CONCEPT,
+    
+    UNKNOWN;
 }
